b1014f6ea859ce334d283c9e3e2a71d62d170bd6
ankanaghosh2001 <ankanaghosh1111@gmail.com>
2026-05-24 17:23:53 +0530

    added sticker removal test

--- tests/result.e2e.ts
+++ tests/result.e2e.ts
@@ -1,8 +1,10 @@
 import { test, expect } from "@playwright/test";
 
-test("adds sticker to polaroid", async ({ page, browserName }) => {
+test("adds sticker to polaroid", async ({ page, browserName, context }) => {
     
   test.skip(browserName !== "chromium");
+
+  await context.grantPermissions(["camera"]);
 
   await page.goto("/camera?shotsCount=1");
 
@@ -25,9 +27,11 @@ test("adds sticker to polaroid", async ({ page, browserName }) => {
   await expect(page.locator('[data-testid="added-sticker"]')).toBeVisible();
 });
 
-test("sticker can be dragged", async ({ page, browserName }) => {
+test("sticker can be dragged", async ({ page, browserName, context }) => {
 
   test.skip(browserName !== "chromium");
+
+  await context.grantPermissions(["camera"]);
 
   await page.goto("/camera?shotsCount=1");
 
@@ -45,3 +49,25 @@ test("sticker can be dragged", async ({ page, browserName }) => {
 
   await expect(sticker).toBeVisible();
 });
+
+test('sticker is removed upon clicking the x button', async ({ page, browserName, context }) => {
+  test.skip(browserName !== "chromium");
+
+  await context.grantPermissions(["camera"]);
+
+  await page.goto("/camera?shotsCount=1"); 
+
+  await page.getByRole("button", { name: "Take Image", exact: true }).click();
+
+  await page.getByRole("button", { name: "Customize My Photos" }).click(); 
+
+  await expect(page).toHaveURL(/result/);
+
+  await page.getByRole("button", {name: /sticker/i,}).first().click();
+
+  await expect(page.locator('[data-testid="added-sticker"]')).toBeVisible();
+
+  await page.locator('.sticker-delete-button').click();
+
+  await expect(page.locator('[data-testid="added-sticker"]')).not.toBeVisible();
+})
